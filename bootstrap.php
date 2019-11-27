018fe4a6c24733dcbd32ef8d82fed6d232f6cb49
Antoine Chassagne <antoine.chassagne@me.com>
2019-11-28 00:50:13 +0100

    :truck: Improve files architecture

--- bootstrap.php
+++ bootstrap.php
@@ -2,12 +2,12 @@
 
 require_once('CLIUtil.php');
 require_once('Game.php');
-require_once('Item.php');
 require_once('Map.php');
-require_once('Player.php');
-require_once('Bonuses/Armor.php');
-require_once('Bonuses/Potion.php');
-require_once('Enemies/Enemy.php');
-require_once('Enemies/Hunter.php');
-require_once('Enemies/Mage.php');
-require_once('Enemies/Warrior.php');
+require_once('Items/Item.php');
+require_once('Items/Player.php');
+require_once('Items/Bonuses/Armor.php');
+require_once('Items/Bonuses/Potion.php');
+require_once('Items/Enemies/Enemy.php');
+require_once('Items/Enemies/Hunter.php');
+require_once('Items/Enemies/Mage.php');
+require_once('Items/Enemies/Warrior.php');
